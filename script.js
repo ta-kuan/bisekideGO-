d12dbed268fca941d8fa35c9e2302a8adc1bb959
ta-kuan <176125305@neg.edu.pref.shizuoka.jp>
2026-03-17 11:33:04 +0900

    Update script.js

--- script.js
+++ script.js
@@ -26,9 +26,7 @@ function create() {
   const height = this.scale.height;
   
   let bi1 = this.add.image(0,0,'biB')
-    .setOrigin(0)
-    .setDisplaySize(width, height)
-    .setAlpha(0);
+    .setOrigin(0);
 
   const back = this.add.rectangle(0,0,width,height,0xffffff)
     .setOrigin(0);
