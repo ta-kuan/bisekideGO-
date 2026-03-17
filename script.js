a2c029f48dd2760393a6a56960e4272adee8abed
ta-kuan <176125305@neg.edu.pref.shizuoka.jp>
2026-03-17 09:53:48 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+//64-20,48-15
+const config = {
+  type: Phaser.AUTO,
+  pixelArt: true,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: 1280,
+    height: 720
+  },
+  scene: {
+    preload: preload,
+    create: create
+  }
+};
 
+const game = new Phaser.Game(config);
+
+function preload() {
+  this.load.image('', '.png');
+}
+
+function create() {
+
+  const width = this.scale.width;
+  const height = this.scale.height;
+
+}
